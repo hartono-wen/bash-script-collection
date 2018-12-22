4221acea72063d3ca1ad8625780c73eab53a04f1
Hartono Wen <hartono.wen@gmail.com>
2018-12-22 15:40:25 +0700

    (Add) autoclean command

--- system/update-and-upgrade.sh
+++ system/update-and-upgrade.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 sudo apt-get update -y;
 sudo apt-get upgrade -y;
 sudo apt-get full-upgrade -y;
 sudo apt-get dist-upgrade -y;
 sudo apt-get autoremove -y;
+sudo apt-get autoclean -y;
