87184b09cfb68991e85d28f75b47ca61577e015a
Hartono Wen <hartono.wen@gmail.com>
2018-12-22 15:44:22 +0700

    (Add) autoclean command

--- aws/ec2/setup-aws-ec2-with-xubuntu.sh
+++ aws/ec2/setup-aws-ec2-with-xubuntu.sh
@@ -13,12 +13,13 @@ sudo apt-get upgrade -y;
 sudo apt-get full-upgrade -y;
 sudo apt-get dist-upgrade -y;
 sudo apt-get autoremove -y;
+sudo apt-get autoclean -y;
 
 sudo sed -i 's\PasswordAuthentication no\PasswordAuthentication yes\g' /etc/ssh/sshd_config;
 
 sudo /etc/init.d/ssh restart;
 
-echo -e "$SERVER_PASSWORD\n$SERVER_PASSWORD" | passwd ubuntu
+sudo echo -e "$SERVER_PASSWORD\n$SERVER_PASSWORD" | passwd ubuntu
 
 sudo apt-get install xrdp nano xubuntu-desktop -y;
 
@@ -26,6 +27,6 @@ echo xfce4-session >~/.xsession;
 
 sudo cp /home/ubuntu/.xsession /etc/skel;
 
-sed -i 's\port=-1\port=ask-1\g' /etc/xrdp/xrdp.ini;
+sudo sed -i 's\port=-1\port=ask-1\g' /etc/xrdp/xrdp.ini;
 
 sudo service xrdp restart;
