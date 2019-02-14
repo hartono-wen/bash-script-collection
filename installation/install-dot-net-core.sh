fbd9f3ec05aa6b719de637719dba218d95942d0c
vm-ubuntu-user <hartono.wen@gmail.com>
2019-02-14 01:10:27 -0500

    (Add) bash script to install .NET Core

---
+++ installation/install-dot-net-core.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INSTALLED_MODULE_NAME=".NET Core";
+
+echo '==='$INSTALLED_MODULE_NAME'=============';
+sudo apt-get update;
+
+echo '===Install the needed application=============';
+sudo apt-get install -y wget apt-transport-https;
+
+echo '===Download package installer for '$INSTALLED_MODULE_NAME'=============';
+sudo wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb;
+
+echo '===Install '$INSTALLED_MODULE_NAME' package using dpkg=============';
+sudo dpkg -i packages-microsoft-prod.deb;
+
+echo '===Update the apt package index=============';
+sudo apt-get update;
+
+echo '===Install PostgreSQL=============';
+sudo apt-get install -y dotnet-sdk-2.1;
+
+echo '===Remove unneeded artifacts=============';
+sudo rm packages-microsoft-prod.deb;
+
+echo '===Finish instaling '$INSTALLED_MODULE_NAME'=============';
+
+echo '===Check '$INSTALLED_MODULE_NAME' version=============';
+dotnet --version
