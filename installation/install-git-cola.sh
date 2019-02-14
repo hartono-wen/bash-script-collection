61638fe1b1524ae1ee3a95cdc1272fad7cddd54c
vm-ubuntu-user <hartono.wen@gmail.com>
2019-02-14 00:55:34 -0500

    (Fix) some wordings for better readability

--- installation/install-git-cola.sh
+++ installation/install-git-cola.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+INSTALLED_MODULE_NAME="Git Cola";
+
+echo '===Start installing '$INSTALLED_MODULE_NAME'=============';
+echo '===Update the apt package index=============';
 sudo apt-get update;
+
+echo '===Install '$INSTALLED_MODULE_NAME'=============';
 sudo apt-get install -y git-cola;
+
+echo '===Finish instaling '$INSTALLED_MODULE_NAME'=============';
+
+echo '===Check '$INSTALLED_MODULE_NAME' version=============';
+git-cola --version
