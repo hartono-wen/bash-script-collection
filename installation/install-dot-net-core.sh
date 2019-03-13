038187c09a0ad598f04dd07a1e6dd6fba2510c4d
hartono.wen <hartono.wen@gmail.com>
2019-03-13 18:40:14 +0700

    (Fix) some wordings for better readability

--- installation/install-dot-net-core.sh
+++ installation/install-dot-net-core.sh
@@ -4,6 +4,10 @@ INSTALLED_MODULE_NAME=".NET Core";
 
 echo '===Start installing '$INSTALLED_MODULE_NAME'=============';
 
+echo '===Set up the variables for later usage=============';
+DOT_NET_PACKAGE_NAME="packages-microsoft-prod.deb";
+DOT_NET_PACKAGE_DOWNLOAD_URL="https://packages.microsoft.com/config/ubuntu/16.04/$DOT_NET_PACKAGE_NAME";
+
 echo '===Update the apt package index=============';
 sudo apt-get update;
 
@@ -11,10 +15,10 @@ echo '===Install the needed application=============';
 sudo apt-get install -y wget apt-transport-https;
 
 echo '===Download '$INSTALLED_MODULE_NAME$'\'s package installer=============';
-sudo wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb;
+sudo wget -nc $DOT_NET_PACKAGE_DOWNLOAD_URL;
 
 echo '===Install '$INSTALLED_MODULE_NAME' package using dpkg=============';
-sudo dpkg -i packages-microsoft-prod.deb;
+sudo dpkg -i $DOT_NET_PACKAGE_NAME;
 
 echo '===Update the apt package index=============';
 sudo apt-get update;
@@ -23,9 +27,9 @@ echo '===Install '$INSTALLED_MODULE_NAME'=============';
 sudo apt-get install -y dotnet-sdk-2.1;
 
 echo '===Remove unneeded artifacts=============';
-sudo rm packages-microsoft-prod.deb;
+sudo rm $DOT_NET_PACKAGE_NAME;
 
 echo '===Finish instaling '$INSTALLED_MODULE_NAME'=============';
 
 echo '===Check '$INSTALLED_MODULE_NAME' version=============';
-dotnet --version
+dotnet --version;
